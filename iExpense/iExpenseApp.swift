ed0915126a635e331e0b892c30ffbf77e8ba89a5
Antonio Giroz <antonioluisgil@gmail.com>
2025-10-11 08:30:04 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Antonio Giroz on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
